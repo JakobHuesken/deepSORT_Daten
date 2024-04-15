6f1d38a37c767ed6d6ca6ed0bf69b029be8243d8
Jakob Hüsken <jakob.h.f.huesken@gmail.com>
2024-04-15 10:55:44 +0200

    first commit

---
+++ Datenklau.sh
@@ -0,0 +1,4 @@
+cp /usr/local/src/git/tkDNN/build/calib* ~/Desktop/Datenerhebung_DeepSort/Tests
+cp /usr/local/src/git/tkDNN/deep_sort/tmp/* ~/Desktop/Datenerhebung_DeepSort/Tests
+cp /usr/local/src/git/tkDNN/deep_sort/resources/detections/test/* ~/Desktop/Datenerhebung_DeepSort/Tests
+
